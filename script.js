d776de241ee1da5831df308291a7a288d0c28b2d
Vinicius Martins <vinicius.stmartins@gmail.com>
2023-06-20 11:23:51 -0300

    teste2

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
+function atualizarLista1() {
+  var lista1 = document.getElementById("lista1").value;
+  var numerosLista1 = lista1.split("\n").map(Number);
+
+  // Atualizar quantidade de itens
+  document.getElementById("qtdLista1").textContent = numerosLista1.length;
+
+  // Remover destaque vermelho dos números existentes
+  var elementosLista1 = document.querySelectorAll("#lista1");
+  for (var i = 0; i < elementosLista1.length; i++) {
+    elementosLista1[i].classList.remove("vermelho");
+  }
+}
+
 function compararListas() {
   var lista1 = document.getElementById("lista1").value;
   var lista2 = document.getElementById("lista2").value;
 
-  var numeros1 = lista1.split("\n").map(Number);
-  var numeros2 = lista2.split("\n").map(Number);
+  var numerosLista1 = lista1.split("\n").map(Number);
+  var numerosLista2 = lista2.split("\n").map(Number);
 
   var numerosAusentes = [];
 
-  for (var i = 0; i < numeros1.length; i++) {
-    if (!numeros2.includes(numeros1[i])) {
-      numerosAusentes.push(numeros1[i]);
+  for (var i = 0; i < numerosLista1.length; i++) {
+    if (!numerosLista2.includes(numerosLista1[i])) {
+      numerosAusentes.push(numerosLista1[i]);
     }
   }
 
-  for (var j = 0; j < numeros2.length; j++) {
+  for (var j = 0; j < numerosLista2.length; j++) {
     if (
-      !numeros1.includes(numeros2[j]) &&
-      !numerosAusentes.includes(numeros2[j])
+      !numerosLista1.includes(numerosLista2[j]) &&
+      !numerosAusentes.includes(numerosLista2[j])
     ) {
-      numerosAusentes.push(numeros2[j]);
+      numerosAusentes.push(numerosLista2[j]);
     }
   }
 
   document.getElementById("numerosAusentes").value = numerosAusentes.join("\n");
 
   // Atualizar quantidade de itens
-  document.getElementById("qtdLista1").textContent = numeros1.length;
-  document.getElementById("qtdLista2").textContent = numeros2.length;
+  document.getElementById("qtdLista2").textContent = numerosLista2.length;
   document.getElementById("qtdNumerosAusentes").textContent =
     numerosAusentes.length;
 
-  // Adicionar classes de estilo aos elementos da lista 1
+  // Destacar números ausentes em vermelho na Lista 1
   var elementosLista1 = document.querySelectorAll("#lista1");
   for (var k = 0; k < elementosLista1.length; k++) {
-    var numero = elementosLista1[k].value;
+    var numero = elementosLista1[k].textContent;
     if (numerosAusentes.includes(Number(numero))) {
-      elementosLista1[k].classList.add("amarelo");
-      elementosLista1[k].classList.remove("verde");
-    } else if (numeros2.includes(Number(numero))) {
-      elementosLista1[k].classList.add("verde");
-      elementosLista1[k].classList.remove("amarelo");
+      elementosLista1[k].classList.add("vermelho");
     } else {
-      elementosLista1[k].classList.remove("verde", "amarelo");
+      elementosLista1[k].classList.remove("vermelho");
     }
   }
 }
